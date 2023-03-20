b73e7d9feb99b9ea4b339cc56621c486579aed1c
Danil Markov <markov.danil.04@mail.ru>
2023-03-20 21:58:29 +0300

    Настроил всякие штуки, разбил на компоненты

---
+++ myproject/src/components/Nav.jsx
@@ -0,0 +1,24 @@
+const Nav = () => (
+  <nav className="nav">
+    <div>
+      <a href="https://github.com/Ch1ChaGL">Profile</a>
+    </div>
+    <div>
+      <a href="https://github.com/Ch1ChaGL">Profile</a>
+    </div>
+    <div>
+      <a href="https://github.com/Ch1ChaGL">Messages</a>
+    </div>
+    <div>
+      <a href="https://github.com/Ch1ChaGL">News</a>
+    </div>
+    <div>
+      <a href="https://github.com/Ch1ChaGL">Music</a>
+    </div>
+    <div>
+      <a href="https://github.com/Ch1ChaGL">Settings</a>
+    </div>
+  </nav>
+);
+
+export { Nav };
---
+++ myproject/src/components/Profile.jsx
@@ -0,0 +1,21 @@
+const Profile = () => (
+  <div className="content">
+    <div>
+      <img
+        className="billboard"
+        src="https://bipbap.ru/wp-content/uploads/2017/04/0_7c779_5df17311_orig.jpg"
+      ></img>
+    </div>
+    <div>ava + description</div>
+    <div>
+      my posts
+      <div>new post</div>
+      <div>
+        <div>Post 1</div>
+        <div>Post 2</div>
+      </div>
+    </div>
+  </div>
+);
+
+export { Profile };
